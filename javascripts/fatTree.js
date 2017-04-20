15b1a5899eec77e3e7a94d5757f6bcff31a61d1c
Gabor Brajer <gabor.brajer@gmail.com>
2017-04-20 22:00:03 +0200

    Initial commit; Added graph; Round Rubin start

---
+++ javascripts/fatTree.js
@@ -0,0 +1,404 @@
+let fatTreeData = {
+  "nodes": [
+    {
+      "id": "r",
+      "label": "R",
+      "x": 0.5,
+      "y": 0.1,
+      "size": 1
+    },
+    {
+      "id": "switch_1",
+      "label": "Switch",
+      "x": 0.2,
+      "y": 0.2,
+      "size": 1
+    },
+    {
+      "id": "switch_2",
+      "label": "Switch",
+      "x": 0.4,
+      "y": 0.2,
+      "size": 1
+    },
+    {
+      "id": "switch_3",
+      "label": "Switch",
+      "x": 0.6,
+      "y": 0.2,
+      "size": 1
+    },
+    {
+      "id": "switch_4",
+      "label": "Switch",
+      "x": 0.8,
+      "y": 0.2,
+      "size": 1
+    },
+    {
+      "id": "vswitch_1",
+      "label": "V Switch",
+      "x": 0.1,
+      "y": 0.5,
+      "size": 2
+    },
+    {
+      "id": "vswitch_2",
+      "label": "V Switch",
+      "x": 0.4,
+      "y": 0.5,
+      "size": 2
+    },
+    {
+      "id": "vswitch_3",
+      "label": "V Switch",
+      "x": 0.6,
+      "y": 0.5,
+      "size": 2
+    },
+    {
+      "id": "vswitch_4",
+      "label": "V Switch",
+      "x": 0.9,
+      "y": 0.5,
+      "size": 2
+    },
+    {
+      "id": "tor_1",
+      "label": "TOR",
+      "x": 0.1,
+      "y": 0.6,
+      "size": 1
+    },
+    {
+      "id": "tor_2",
+      "label": "TOR",
+      "x": 0.4,
+      "y": 0.6,
+      "size": 1
+    },
+    {
+      "id": "tor_3",
+      "label": "TOR",
+      "x": 0.6,
+      "y": 0.6,
+      "size": 1
+    },
+    {
+      "id": "tor_4",
+      "label": "TOR",
+      "x": 0.9,
+      "y": 0.6,
+      "size": 1
+    },
+    {
+      "id": "server_1",
+      "label": "<Server>",
+      "x": 0.1,
+      "y": 0.7,
+      "size": 3
+    },
+    {
+      "id": "server_2",
+      "label": "<Server>",
+      "x": 0.1,
+      "y": 0.73,
+      "size": 3
+    },
+    {
+      "id": "server_3",
+      "label": "<Server>",
+      "x": 0.1,
+      "y": 0.76,
+      "size": 3
+    },
+    {
+      "id": "server_4",
+      "label": "<Server>",
+      "x": 0.1,
+      "y": 0.79,
+      "size": 3
+    },
+    {
+      "id": "server_5",
+      "label": "<Server>",
+      "x": 0.4,
+      "y": 0.7,
+      "size": 3
+    },
+    {
+      "id": "server_6",
+      "label": "<Server>",
+      "x": 0.4,
+      "y": 0.73,
+      "size": 3
+    },
+    {
+      "id": "server_7",
+      "label": "<Server>",
+      "x": 0.4,
+      "y": 0.76,
+      "size": 3
+    },
+    {
+      "id": "server_8",
+      "label": "<Server>",
+      "x": 0.4,
+      "y": 0.79,
+      "size": 3
+    },
+    {
+      "id": "server_9",
+      "label": "<Server>",
+      "x": 0.6,
+      "y": 0.7,
+      "size": 3
+    },
+    {
+      "id": "server_10",
+      "label": "<Server>",
+      "x": 0.6,
+      "y": 0.73,
+      "size": 3
+    },
+    {
+      "id": "server_11",
+      "label": "<Server>",
+      "x": 0.6,
+      "y": 0.76,
+      "size": 3
+    },
+    {
+      "id": "server_12",
+      "label": "<Server>",
+      "x": 0.6,
+      "y": 0.79,
+      "size": 3
+    },
+    {
+      "id": "server_13",
+      "label": "<Server>",
+      "x": 0.9,
+      "y": 0.7,
+      "size": 3
+    },
+    {
+      "id": "server_14",
+      "label": "<Server>",
+      "x": 0.9,
+      "y": 0.73,
+      "size": 3
+    },
+    {
+      "id": "server_15",
+      "label": "<Server>",
+      "x": 0.9,
+      "y": 0.76,
+      "size": 3
+    },
+    {
+      "id": "server_16",
+      "label": "<Server>",
+      "x": 0.9,
+      "y": 0.79,
+      "size": 3
+    }
+  ],
+  "edges": [
+    {
+      "id": "e0",
+      "source": "r",
+      "target": "switch_1"
+    },
+    {
+      "id": "e1",
+      "source": "r",
+      "target": "switch_2"
+    },
+    {
+      "id": "e2",
+      "source": "r",
+      "target": "switch_3"
+    },
+    {
+      "id": "e3",
+      "source": "r",
+      "target": "switch_4"
+    },
+    {
+      "id": "e4",
+      "source": "switch_1",
+      "target": "vswitch_1"
+    },
+    {
+      "id": "e5",
+      "source": "switch_1",
+      "target": "vswitch_2"
+    },
+    {
+      "id": "e6",
+      "source": "switch_1",
+      "target": "vswitch_3"
+    },
+    {
+      "id": "e7",
+      "source": "switch_1",
+      "target": "vswitch_4"
+    },
+    {
+      "id": "e8",
+      "source": "switch_2",
+      "target": "vswitch_1"
+    },
+    {
+      "id": "e9",
+      "source": "switch_2",
+      "target": "vswitch_2"
+    },
+    {
+      "id": "e10",
+      "source": "switch_2",
+      "target": "vswitch_3"
+    },
+    {
+      "id": "11",
+      "source": "switch_2",
+      "target": "vswitch_4"
+    },
+    {
+      "id": "e12",
+      "source": "switch_3",
+      "target": "vswitch_1"
+    },
+    {
+      "id": "e13",
+      "source": "switch_3",
+      "target": "vswitch_2"
+    },
+    {
+      "id": "e14",
+      "source": "switch_3",
+      "target": "vswitch_3"
+    },
+    {
+      "id": "e15",
+      "source": "switch_3",
+      "target": "vswitch_4"
+    },
+    {
+      "id": "e16",
+      "source": "switch_4",
+      "target": "vswitch_1"
+    },
+    {
+      "id": "e17",
+      "source": "switch_4",
+      "target": "vswitch_2"
+    },
+    {
+      "id": "e18",
+      "source": "switch_4",
+      "target": "vswitch_3"
+    },
+    {
+      "id": "e19",
+      "source": "switch_4",
+      "target": "vswitch_4"
+    },
+    {
+      "id": "e20",
+      "source": "vswitch_1",
+      "target": "tor_1"
+    },
+    {
+      "id": "e21",
+      "source": "vswitch_2",
+      "target": "tor_2"
+    },
+    {
+      "id": "e22",
+      "source": "vswitch_3",
+      "target": "tor_3"
+    },
+    {
+      "id": "e23",
+      "source": "vswitch_4",
+      "target": "tor_4"
+    },
+    {
+      "id": "e28",
+      "source": "tor_1",
+      "target": "server_1"
+    },
+    {
+      "id": "e29",
+      "source": "tor_2",
+      "target": "server_5"
+    },
+    {
+      "id": "e30",
+      "source": "tor_3",
+      "target": "server_9"
+    },
+    {
+      "id": "e31",
+      "source": "tor_4",
+      "target": "server_13"
+    },
+    {
+      "id": "e32",
+      "source": "server_1",
+      "target": "server_2"
+    },
+    {
+      "id": "e33",
+      "source": "server_2",
+      "target": "server_3"
+    },
+    {
+      "id": "e34",
+      "source": "server_3",
+      "target": "server_4"
+    },
+    {
+      "id": "e35",
+      "source": "server_5",
+      "target": "server_6"
+    },
+    {
+      "id": "e36",
+      "source": "server_6",
+      "target": "server_7"
+    },
+    {
+      "id": "e37",
+      "source": "server_7",
+      "target": "server_8"
+    },
+    {
+      "id": "e38",
+      "source": "server_9",
+      "target": "server_10"
+    },
+    {
+      "id": "e39",
+      "source": "server_10",
+      "target": "server_11"
+    },
+    {
+      "id": "e40",
+      "source": "server_11",
+      "target": "server_12"
+    },
+    {
+      "id": "e41",
+      "source": "server_13",
+      "target": "server_14"
+    },
+    {
+      "id": "e42",
+      "source": "server_15",
+      "target": "server_16"
+    }
+  ]
+}
